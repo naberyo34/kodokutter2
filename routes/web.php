ac8a849891a42fcdecf12526d9e08c09bacd8471
Ryo <naberyo34@gmail.com>
2022-07-09 23:41:47 +0900

    add: chap2, Viewを返すまで

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class, 'show']);
+
+Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class, 'showId']);
+
+// シングルアクションコントローラを呼び出す場合は、メソッド名を指定しなくてよい
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class);
